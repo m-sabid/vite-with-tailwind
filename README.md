1ac7bd1fad6830417267e0a49467a67fafdb34c1
m-sabid <sabidofficial@gmail.com>
2023-03-29 17:42:04 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vite-with-tailwind
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-600 text-center text-2xl font-bold text-white">Tailwind CSS</h1>
+    </>
+  );
+}
+
+export default App;
